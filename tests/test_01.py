5d8a50120b453e1102d4381b7d31ca49007ce425
msh <two867@qq.com>
2026-05-16 16:28:03 +0800

    allure title

--- tests/test_01.py
+++ tests/test_01.py
@@ -1,14 +1,16 @@
 import pytest
+import allure
 from playwright.sync_api import Page, expect
 
 
-
+@allure.title("自动化-悬浮")
 def test_hover_01(page: Page):
     page.goto("http://www.xn--6frwj470ei1s2kl.com/demo/hover")
     page.get_by_role("button").hover()
     expect(page.locator("#popup")).to_have_text("你已经成功悬浮")
 
 
+@allure.title("访问百度")
 def test_baidu_01(page: Page):
     page.goto("https://www.baidu.com")
     expect(page).to_have_title("百度一下，你就知道")
